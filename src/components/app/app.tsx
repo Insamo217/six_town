aca599106c8b4c55a06ddf113b85e8242e0adc45
Insamo <insamo217@yandex.ru>
2023-04-07 20:54:04 +0300

    Добавление новых страниц

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Main from "../main/main";
+import Main from "../../pages/main/main";
 
 function App({ countPlaces }: { countPlaces: number }): JSX.Element {
   return <Main countPlaces={countPlaces} />;
